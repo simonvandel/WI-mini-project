7df598fa0b83b07ec01af80c388e46feeefc181b
Michael <micjen12@student.aau.dk>
2016-09-12 15:00:12 +0200

    changes

---
+++ web-crawler/src/main/java/crawlerCache.java
@@ -0,0 +1,29 @@
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.LoadingCache;
+import crawlercommons.robots.BaseRobotRules;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Michael on 12-09-2016.
+ */
+public class crawlerCache {
+
+    private Cache<String, BaseRobotRules> robotcache = CacheBuilder.newBuilder().expireAfterWrite(10, TimeUnit.MINUTES).build();
+
+    public BaseRobotRules getRobotTxt(URL url) {
+        BaseRobotRules result = robotcache.getIfPresent(url.getHost());
+        if (result != null) {
+            try {
+                String robottxt = utilities.downloadText(url.getHost() + "/robots.txt");
+                result = RobotsTxtParser.parse(robottxt.getBytes(), "NiceBot");
+                robotcache.put(url.getHost(), result);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
