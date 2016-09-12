188ec12f8dca321f25d98890d9099e9c2efb7921
Michael <micjen12@student.aau.dk>
2016-09-12 12:03:09 +0200

    Adding constructors to crawlerqueue

--- web-crawler/src/main/java/crawlerQueue.java
+++ web-crawler/src/main/java/crawlerQueue.java
@@ -1,7 +1,16 @@
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 public class crawlerQueue {
+
+    public crawlerQueue() {
+
+    }
+
+    public crawlerQueue(List<URI> seed) {
+
+    }
 
     public static URI fetchNextURI() throws URISyntaxException {
         return new URI("http://www.google.dk");
