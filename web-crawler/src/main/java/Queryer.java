76fc4cfe139577eba4283c0774d48a5f53f94886
Simon Vandel Sillesen <simon.vandel@gmail.com>
2016-09-26 14:35:49 +0200

    repl

--- web-crawler/src/main/java/Queryer.java
+++ web-crawler/src/main/java/Queryer.java
@@ -2,10 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -13,11 +10,6 @@ import java.util.stream.Collectors;
  */
 public class Queryer {
     public static void main(String[] args) {
-        // read args for query
-        List<String> query = Tokenizer.tokenize(String.join(" ", args));
-
-        System.out.println(query);
-
         // read indices
         Path startPath = Paths.get("indices/");
         // key is token, value is list of urlHash + count
@@ -27,33 +19,32 @@ public class Queryer {
         final int[] N = {0};
         try {
             Files.walk(startPath)
-                // skip the directory
-                .skip(1)
-                .forEach(path -> {
-                    // key is urlHash, value is tf-idf
-                    HashMap<String, ValueHolder> innerHashmap = new HashMap<>();
-                    N[0]++;
-                    // parse file
-                    try {
-                        Files.readAllLines(path)
-                                // go through each url hash
-                            .forEach(line -> {
-                                String[] split = line.split(" ");
-                                String urlHash = split[0];
-                                Integer count = Integer.parseInt(split[1]);
-                                Double tdIdf = Double.parseDouble(split[2]);
-                                innerHashmap.put(urlHash, new ValueHolder(count, tdIdf));
-                            });
-                        termInfo.put(path.getFileName().toString(), innerHashmap);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
+                    // skip the directory
+                    .skip(1)
+                    .forEach(path -> {
+                        // key is urlHash, value is tf-idf
+                        HashMap<String, ValueHolder> innerHashmap = new HashMap<>();
+                        N[0]++;
+                        // parse file
+                        try {
+                            Files.readAllLines(path)
+                                    // go through each url hash
+                                    .forEach(line -> {
+                                        String[] split = line.split(" ");
+                                        String urlHash = split[0];
+                                        Integer count = Integer.parseInt(split[1]);
+                                        Double tdIdf = Double.parseDouble(split[2]);
+                                        innerHashmap.put(urlHash, new ValueHolder(count, tdIdf));
+                                    });
+                            termInfo.put(path.getFileName().toString(), innerHashmap);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println("test1");
         // populate tfHashMap
         termInfo.forEach((term, innerHashmap) -> {
             innerHashmap.forEach((document, valueHolder) -> {
@@ -66,38 +57,53 @@ public class Queryer {
             });
         });
 
-        System.out.println("test2");
-        HashMap<String, Double> scores = new HashMap<>();
+        long start = 0;
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter query:");
+            // read query from stdin
+            String line = sc.nextLine();
+            start = System.currentTimeMillis();
+            List<String> query = Tokenizer.tokenize(String.join(" ", line));
 
-        for (String term : query) {
+            HashMap<String, Double> scores = new HashMap<>();
 
-            HashMap<String, ValueHolder> urlHashMap = termInfo.get(term);
-            urlHashMap.forEach((urlHash, valueHolder) -> {
-                scores.putIfAbsent(urlHash, 0.0);
+            for (String term : query) {
 
-                scores.compute(urlHash, (x, oldCount) -> oldCount + valueHolder.tfidf);
+                HashMap<String, ValueHolder> urlHashMap = termInfo.get(term);
+                if(urlHashMap == null) continue;
+                urlHashMap.forEach((urlHash, valueHolder) -> {
+                    scores.putIfAbsent(urlHash, 0.0);
+
+                    scores.compute(urlHash, (x, oldCount) -> oldCount + valueHolder.tfidf);
+                });
+            }
+
+            // normalize score
+            scores.forEach((document, score) -> {
+                double length = Math.sqrt(tfHashMap.get(document).values()
+                        .stream().map(x -> x * x).mapToInt(Integer::intValue).sum());
+                scores.compute(document, (x, oldScore) -> oldScore / length);
             });
+
+            scores.entrySet()
+                    .stream()
+                    .sorted((e1, e2) -> Double.compare(e2.getValue(), e1.getValue()))
+                    .map(e -> {
+                        try {
+                            String url = Files.readAllLines(Paths.get("storedPages/" + e.getKey())).get(0);
+                            return url + " " + e.getValue();
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                            return "error";
+                        }
+                    }).forEach(System.out::println);
+
+            if (scores.size() == 0) {
+                System.out.println("No results found");
+            }
+
+            System.out.println("Query took: " + (System.currentTimeMillis()-start) + "ms");
         }
-
-        // normalize score
-        scores.forEach((document, score) -> {
-            double length = Math.sqrt(tfHashMap.get(document).values()
-                    .stream().map(x -> x * x).mapToInt(Integer::intValue).sum());
-            scores.compute(document, (x, oldScore) -> oldScore / length);
-        });
-
-        scores.entrySet()
-                .stream()
-                .sorted((e1, e2) -> Double.compare(e2.getValue(), e1.getValue()))
-                .map(e -> {
-                    try {
-                        String url = Files.readAllLines(Paths.get("storedPages/" + e.getKey())).get(0);
-                        return url + " " + e.getValue();
-                    } catch (IOException e1) {
-                        e1.printStackTrace();
-                        return "error";
-                    }
-                }).forEach(x -> System.out.println(x));
-        ;
     }
 }
